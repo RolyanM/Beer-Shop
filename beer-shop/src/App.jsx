f8ad51be242738eb7c8a8823f50975aa89e872e0
Martin Naylor <martinnaylor88@gmail.com>
2021-07-17 11:17:09 +0100

    api but not showing on page

--- beer-shop/src/App.jsx
+++ beer-shop/src/App.jsx
@@ -1,7 +1,30 @@
 
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.module.scss";
 
 function App() {
+
+  const [beers, setBeers] = useState([]);
+
+
+
+
+    // Fetch the beers from PUNK API
+    const fetchBeers = () => {
+      fetch("https://api.punkapi.com/v2/beers")
+        // Put response into response.json file
+        .then((response) => response.json())
+        // Add beers received in API fetch to beers object array of setBeers
+        .then((data) => setBeers(data))
+        // Catch errors
+        .catch((error) => console.log(error));
+    };
+  
+    // On page load, fetch the beers
+    useEffect(() => {
+      fetchBeers();
+    }, []);
+
   return (
     <div className="App">
   
